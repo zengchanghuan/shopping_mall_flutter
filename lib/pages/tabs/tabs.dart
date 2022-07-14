043a40639a285cddcec3376c1584c5d797d70cf4
zengchanghuan <zengchanghuan@gmail.com>
2022-07-14 15:19:10 +0800

    项目框架搭建，路由配置

---
+++ lib/pages/tabs/tabs.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'home.dart';
+import 'cart.dart';
+import 'category.dart';
+import 'user.dart';
+class Tabs extends StatefulWidget {
+  const Tabs({Key? key}) : super(key: key);
+
+  @override
+  State<Tabs> createState() => _TabsState();
+}
+
+class _TabsState extends State<Tabs> {
+  int _currentIndex = 0;
+  final List _pageList = [
+    const HomePage(),
+    const CategoryPage(),
+    const CartPage(),
+    const UserPage()
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("shopping mall"),
+      ),
+      body: _pageList[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: (index){
+          setState((){
+            _currentIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        fixedColor: Colors.red,
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: "首页"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: "分类"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.shopping_cart),
+              label: "购物车"
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(Icons.people),
+              label: "我的"
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/routers/router.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import '../pages/tabs/Tabs.dart';
+
+//配置路由
+final Map<String, Function> routes = {
+  '/': (context) => const Tabs(),
+};
+
+//固定写法
+var onGenerateRoute = (RouteSettings settings) {
+  final String? name = settings.name;
+  final Function? pageContentBuilder = routes[name];
+  if (pageContentBuilder != null) {
+    if (settings.arguments != null) {
+      final Route route = MaterialPageRoute(
+          builder: (context) =>
+              pageContentBuilder(context, arguments: settings.arguments));
+      return route;
+    } else {
+      final Route route =
+          MaterialPageRoute(builder: (context) => pageContentBuilder(context));
+      return route;
+    }
+  } else {
+    return null;
+  }
+};
