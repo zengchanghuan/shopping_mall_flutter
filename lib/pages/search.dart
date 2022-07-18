899d3b39ae0deb11976f99bf760b972e7fd1b85a
zengchanghuan <zengchanghuan@gmail.com>
2022-07-18 10:21:56 +0800

    搜索导航自定义

--- lib/pages/search.dart
+++ lib/pages/search.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import '../serivces/screen_adapter.dart';
 class Search extends StatefulWidget {
   const Search({Key? key}) : super(key: key);
 
@@ -12,9 +12,36 @@ class _SearchState extends State<Search> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("search"),
+        title: Container(
+          height: ScreenAdapter.height(68),
+          decoration: BoxDecoration(
+              color: const Color.fromRGBO(233, 233, 233, 0.8),
+              borderRadius: BorderRadius.circular(30)),
+          child: TextField(
+            autofocus: true,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                    borderSide: BorderSide.none)),
+          ),
+        ),
+        actions: <Widget>[
+          InkWell(
+            child: SizedBox(
+              height: ScreenAdapter.height(68),
+              width: ScreenAdapter.width(80),
+              child: Row(
+                children: const <Widget>[Text("搜索")],
+              ),
+            ),
+            onTap: (){
+
+
+            },
+          )
+        ],
       ),
-      body: Text("search"),
+      body: Text('搜索'),
     );
   }
 }
