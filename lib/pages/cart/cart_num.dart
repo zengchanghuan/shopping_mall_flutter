67c81d5b6d3055c4c9dc511e1bdfc1eeee51a66d
zengchanghuan <zengchanghuan@gmail.com>
2022-07-25 18:50:57 +0800

    购物车ui布局

--- lib/pages/cart/cart_num.dart
+++ lib/pages/cart/cart_num.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../provider/cart_provider.dart';
+import '../../serivces/screen_adapter.dart';
 
 class CartNum extends StatefulWidget {
   const CartNum({Key? key}) : super(key: key);
@@ -12,10 +13,64 @@ class CartNum extends StatefulWidget {
 class _CartNumState extends State<CartNum> {
   @override
   Widget build(BuildContext context) {
-    var cartProvider = Provider.of<CartProvider>(context);
 
-    return Column(
-      children: [Text("${cartProvider.cartNum}")],
+    return Container(
+      width: ScreenAdapter.width(164),
+      decoration: BoxDecoration(
+        border: Border.all(width: 1,color: Colors.black12),
+      ),
+      child: Row(
+        children: [
+          _leftBtn(),
+          _centerArea(),
+          _rightBtn()
+        ],
+      ),
+    );
+  }
+
+  //左侧按钮
+
+  Widget _leftBtn() {
+    return InkWell(
+      onTap: () {},
+      child: Container(
+        alignment: Alignment.center,
+        width: ScreenAdapter.width(45),
+        height: ScreenAdapter.height(45),
+        child: const Text("-"),
+      ),
+    );
+  }
+
+  //右侧按钮
+  Widget _rightBtn() {
+    return InkWell(
+      onTap: (){
+
+
+      },
+      child: Container(
+        alignment: Alignment.center,
+        width: ScreenAdapter.width(45),
+        height: ScreenAdapter.height(45),
+        child: const Text("+"),
+      ),
+    );
+  }
+
+//中间
+  Widget _centerArea() {
+    return Container(
+      alignment: Alignment.center,
+      width: ScreenAdapter.width(70),
+      decoration: const BoxDecoration(
+          border: Border(
+            left: BorderSide(width: 1, color: Colors.black12),
+            right: BorderSide(width: 1, color: Colors.black12),
+          )),
+      height: ScreenAdapter.height(45),
+      child: const Text("1"),
     );
   }
 }
