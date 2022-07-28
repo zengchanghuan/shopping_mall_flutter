3c35d830089d469f69faa5a11a7da5d7e8de3289
zengchanghuan <zengchanghuan@gmail.com>
2022-07-28 15:55:02 +0800

    合计价格

--- lib/pages/check_out.dart
+++ lib/pages/check_out.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
+import '../provider/cart_provider.dart';
 import '../services/event_bus.dart';
 import '../config/config.dart';
 import '../services/event_bus.dart';
@@ -19,7 +20,8 @@ class CheckOut extends StatefulWidget {
 }
 
 class _CheckOutState extends State<CheckOut> {
-  List _addressList=[];
+  List _addressList = [];
+
   @override
   void initState() {
     super.initState();
@@ -38,22 +40,19 @@ class _CheckOutState extends State<CheckOut> {
     List userinfo = await UserServices.getUserInfo();
 
     // print('1234');
-    var tempJson = {
-      "uid": userinfo[0]["_id"],
-      "salt": userinfo[0]["salt"]
-    };
+    var tempJson = {"uid": userinfo[0]["_id"], "salt": userinfo[0]["salt"]};
     var sign = SignServices.getSign(tempJson);
-    var api = '${Config.domain}api/oneAddressList?uid=${userinfo[0]["_id"]}&sign=${sign}';
+    var api =
+        '${Config.domain}api/oneAddressList?uid=${userinfo[0]["_id"]}&sign=${sign}';
     var response = await Dio().get(api);
     if (kDebugMode) {
       print(response);
     }
     setState(() {
-      _addressList=response.data['result'];
+      _addressList = response.data['result'];
     });
-
-
   }
+
   Widget _checkOutItem(item) {
     return Row(
       children: <Widget>[
@@ -94,6 +93,8 @@ class _CheckOutState extends State<CheckOut> {
   @override
   Widget build(BuildContext context) {
     var checkOutProvider = Provider.of<CheckOutProvider>(context);
+    var cartProvider = Provider.of<CartProvider>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("结算"),
@@ -106,31 +107,34 @@ class _CheckOutState extends State<CheckOut> {
                 color: Colors.white,
                 child: Column(
                   children: <Widget>[
-                    SizedBox(height: 10),
-                    _addressList.length>0?ListTile(
-                      title: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: <Widget>[
-                          Text("${_addressList[0]["name"]}  ${_addressList[0]["phone"]}"),
-                          SizedBox(height: 10),
-                          Text("${_addressList[0]["address"]}"),
-                        ],
-                      ),
-                      trailing: Icon(Icons.navigate_next),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/addressList');
-                      },
-                    ):ListTile(
-                      leading: Icon(Icons.add_location),
-                      title: Center(
-                        child: Text("请添加收货地址"),
-                      ),
-                      trailing: Icon(Icons.navigate_next),
-                      onTap: () {
-                        Navigator.pushNamed(context, '/addressAdd');
-                      },
-                    ),
-                    SizedBox(height: 10),
+                    const SizedBox(height: 10),
+                    _addressList.isNotEmpty
+                        ? ListTile(
+                            title: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                    "${_addressList[0]["name"]}  ${_addressList[0]["phone"]}"),
+                                const SizedBox(height: 10),
+                                Text("${_addressList[0]["address"]}"),
+                              ],
+                            ),
+                            trailing: const Icon(Icons.navigate_next),
+                            onTap: () {
+                              Navigator.pushNamed(context, '/addressList');
+                            },
+                          )
+                        : ListTile(
+                            leading: const Icon(Icons.add_location),
+                            title: const Center(
+                              child: Text("请添加收货地址"),
+                            ),
+                            trailing: const Icon(Icons.navigate_next),
+                            onTap: () {
+                              Navigator.pushNamed(context, '/addressAdd');
+                            },
+                          ),
+                    const SizedBox(height: 10),
                   ],
                 ),
               ),
@@ -140,10 +144,10 @@ class _CheckOutState extends State<CheckOut> {
                 padding: EdgeInsets.all(ScreenAdapter.width(20)),
                 child: Column(
                     children: checkOutProvider.checkOutListData.map((value) {
-                      return Column(
-                        children: <Widget>[_checkOutItem(value), const Divider()],
-                      );
-                    }).toList()),
+                  return Column(
+                    children: <Widget>[_checkOutItem(value), const Divider()],
+                  );
+                }).toList()),
               ),
               const SizedBox(height: 20),
               Container(
@@ -163,7 +167,7 @@ class _CheckOutState extends State<CheckOut> {
             ],
           ),
           Positioned(
-            bottom: 0,
+            bottom: 10,
             width: ScreenAdapter.width(750),
             height: ScreenAdapter.height(100),
             child: Container(
@@ -173,12 +177,13 @@ class _CheckOutState extends State<CheckOut> {
               decoration: const BoxDecoration(
                   color: Colors.white,
                   border:
-                  Border(top: BorderSide(width: 1, color: Colors.black26))),
+                      Border(top: BorderSide(width: 1, color: Colors.black26))),
               child: Stack(
                 children: <Widget>[
-                  const Align(
+                  Align(
                     alignment: Alignment.centerLeft,
-                    child: Text("总价:￥140", style: TextStyle(color: Colors.red)),
+                    child: Text("${cartProvider.allPrice}",
+                        style: const TextStyle(color: Colors.red)),
                   ),
                   Align(
                     alignment: Alignment.centerRight,
@@ -197,4 +202,3 @@ class _CheckOutState extends State<CheckOut> {
     );
   }
 }
-
